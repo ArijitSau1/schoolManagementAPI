5991818a18a9ecdb661931ffb37804b468959152
Arijit Sau <arijitsau45@gmail.com>
2026-06-04 14:26:35 +0530

    add create student api

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -23,6 +23,41 @@ const getStudents = async (req, res) => {
   }
 };
 
+const createStudent = async (req, res) => {
+  try {
+    const { name, email, class_id } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).send({
+        success: false,
+        message: "Please provide name and email",
+      });
+    }
+
+    const db = await connectDB();
+
+    const [data] = await db.query(
+      "INSERT INTO students(name,email,class_id) VALUES(?,?,?)",
+      [name, email, class_id || null]
+    );
+
+    res.status(201).send({
+      success: true,
+      message: "Student Created Successfully",
+      data,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getStudents,
+  createStudent,
 };
--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -2,10 +2,13 @@ const express = require("express");
 
 const {
   getStudents,
+   createStudent,
 } = require("../controllers/studentController");
 
 const router = express.Router();
 
 router.get("/getall", getStudents);
+
+router.post("/create", createStudent);
 
 module.exports = router;
